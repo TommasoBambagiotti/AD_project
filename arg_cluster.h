70f78d29018aec96bcb6d493caac4a52a1c67a9d
TommasoBambagiotti <tommaso.bambagiotti@studio.unibo.it>
2020-03-03 21:28:19 +0100

    added description

--- arg_cluster.h
+++ arg_cluster.h
@@ -1,24 +1,15 @@
 #include "rt/rt_api.h"
 
-/*
-typedef struct ArgNN {
 
-	rt_perf_t*		perf;
-	unsigned int*		PSD_L1;
-
-
-}ArgNN_t
-*/
-
-
+//Structure needed for pwelch cluster call 
 typedef struct ArgCluster {
-        unsigned short *        In;
-        signed short*           In_FFT;
-        unsigned short*         w_ham;
-        unsigned int*           PSD;
-        short*                  Twiddles;
+        unsigned short *        In; //input samples
+        signed short*           In_FFT; //FFT input samples
+        unsigned short*         w_ham; //hamming window samples
+        unsigned int*           PSD; //PSD array
+        short*                  Twiddles; 
         short*                  SwapTable;
-	rt_perf_t*		welch_perf;
+	rt_perf_t*		welch_perf; //performance counter
 //	ArgNN_t			Cluster_NN;
 }ArgCluster_t;
 
